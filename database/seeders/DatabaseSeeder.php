a60743d585d1e05c944853322a6a6962c7c64a8d
Christopher <102330198+RiperPro03@users.noreply.github.com>
2025-03-05 23:28:38 +0100

    add Role and Permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\Offer;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,12 +14,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        
-        $this->call(PromotionSeeder::class);
-        $this->call(UserSeeder::class);
-        $this->call(CompanySeeder::class);
-        $this->call(SkillSeeder::class);
-        $this->call(OfferSeeder::class);
-        
+
+//        $this->call(PromotionSeeder::class);
+//        $this->call(UserSeeder::class);
+//        $this->call(CompanySeeder::class);
+//        $this->call(SkillSeeder::class);
+//        $this->call(OfferSeeder::class);
+
+        // Création des rôles
+        $roles = ['user', 'student', 'pilot', 'admin'];
+
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+
+        // Création des permissions
+        $permissions = [
+            'access_dashboard',
+            'manage_students',
+            'manage_pilots',
+            'access_student',
+            'access_pilot',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        // Attribution des permissions aux rôles
+        Role::findByName('user')->givePermissionTo([]);
+        Role::findByName('student')->givePermissionTo(['access_student']);
+        Role::findByName('pilot')->givePermissionTo(['manage_students', 'access_pilot']);
+        Role::findByName('admin')->givePermissionTo(Permission::all());
+
+        // Création d'un utilisateur admin
+        $admin = User::factory()->create([
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password')
+        ]);
+        $admin->assignRole('admin');
+
     }
 }
