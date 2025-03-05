5fa064793166899d34f8d62ddf7a2005c65134b2
QuentinGH <quentin.chabres@gmail.com>
2025-03-05 09:16:50 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login.show');
+})->name('login');
+
+Route::get('/dashboard', function () {
+    return view('dashboard.show');
+})->name('dashboard');
+
+Route::get('/profile', function () {
+    return view('profile.show');
+})->name('profile');
+
+Route::get('/student_crud', function () {
+    return view('student_crud.show');
+})->name('student_crud');
