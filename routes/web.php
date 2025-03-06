8be37dc7539b48fffa8491cc7ddb2aad7eb4f351
QuentinGH <quentin.chabres@gmail.com>
2025-03-06 11:39:28 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,3 +22,34 @@ Route::get('/profile', function () {
 Route::get('/student_crud', function () {
     return view('student_crud.show');
 })->name('student_crud');
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
+
+// Page d'insertion pour les produits
+Route::get('dashboard/home', function() {
+    return view('dashboard.home');
+});
+
+Route::get('/offer/insert', function() {
+    return view('offer.insert');
+});
+
+Route::get('/pilot/insert', function() {
+    return view('pilot.insert');
+});
+
+Route::get('/company/insert', function() {
+    return view('company.insert');
+});
+
+Route::get('/student/insert', function() {
+    return view('student.insert');
+});
+
+Route::get('/apply/insert', function() {
+    return view('apply.insert');
+});
+
+Route::get('/wishlist/insert', function() {
+    return view('wishlist.insert');
+});
