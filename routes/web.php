61a22da09ba76df3d9891e9554f230f09c30f955
Christopher <102330198+RiperPro03@users.noreply.github.com>
2025-03-31 16:32:32 +0200

    Revert "Merge branch 'feature/filament' into CRUD"
    This reverts commit 35dcefe6cc4473d0f48e80e906c81d4816c4d9aa, reversing
    changes made to 9c167cb363bb4dbc15cfc04b4e4049e1799e2eec.

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::middleware(['guest'])->group(function () {
 Route::middleware(['auth'])->group(function () {
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
+
 //    Route::resource('admin/company', CompanyController::class)->names([
 //        'index' => 'admin.company.index',
 //        'show' => 'admin.company.show',
