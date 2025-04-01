1349e20424de4c31ac8fa1e3a16493c217c8b9f8
Christopher <102330198+RiperPro03@users.noreply.github.com>
2025-04-01 14:42:50 +0200

    fix: route

--- routes/web.php
+++ routes/web.php
@@ -70,23 +70,18 @@ Route::middleware(['auth', 'can:manage_students'])->group(function () { // auth:
 
 // Pilot CRUD promotion
 Route::middleware(['auth', 'can:manage_promotion'])->group(function () {
-    Route::resource('pilot/promotion', PromotionController::class);
+
 
 });
 
 Route::middleware(['auth', 'can:manage_students'])->group(function () {
     Route::resource('pilot/industry', IndustryController::class);
-
-});
-Route::middleware(['auth', 'can:manage_students'])->group(function () {
     Route::resource('pilot/skill', SkillController::class);
-
-});
-
-Route::middleware(['auth', 'can:manage_students'])->group(function () {
     Route::resource('pilot/sector', SectorController::class);
+    Route::resource('pilot/promotion', PromotionController::class);
 
 });
+
 Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
 
 // Pilot CRUD companies
